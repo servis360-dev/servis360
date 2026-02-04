5845cb3baa55edcba493f6ce197112f244964b73
Azem <yunus.ahx@gmail.com>
2026-02-05 02:35:38 +0300

    Config ve sayfa duzeltmeleri

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 ﻿import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../../globals.css"; // <--- İŞTE EKSİK OLAN SİHİRLİ SATIR BU!
+import "../globals.css"; // <--- DÜZELTİLDİ: Sadece bir üst klasöre (app/) çıkması yeterli.
 
 const inter = Inter({ subsets: ["latin"] });
 
