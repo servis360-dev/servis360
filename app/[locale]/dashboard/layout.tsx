b4548ed432324a4655c4a7e60fdcdcf726ca85e5
Azem <yunus.ahx@gmail.com>
2026-02-06 01:29:07 +0300

    Config ve sayfa duzeltmeleri

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-﻿import { getDictionary } from '../../../../dictionaries'; // Sözlük yolunu doğru ayarladığından emin ol
+﻿import { getDictionary } from '../../../dictionaries'; // Sözlük yolunu doğru ayarladığından emin ol
 import DashboardShell from './shell';
 
 export default async function DashboardLayout({
